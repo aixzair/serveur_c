d513d46dc73786e6f040855040d9cad2f7ad6b9c
Alexandre Lerosier <alex.lerosier@gmail.com>
2023-12-15 14:55:32 +0100

    create variable

--- src/Serveur2.c
+++ src/Serveur2.c
@@ -13,6 +13,25 @@
 #include "Matiere.h"
 #include "Message.h"
 
+Matiere matieres[] = {
+    { 1,"Anglais\0", 12.5},
+    { 2,"Mathematiques\0", 10.8},
+    { 3,"Informatique\0", 9.3},
+    { 4,"Culturte generale\0", 12.6},
+    { 5,"Physiques\0", 11.8},
+    { 6,"Programmation systeme\0", 12.3}
+};
+
+int socketEcoute;
+int socketDialogue;
+struct sockaddr_in serveur;
+struct sockaddr_in pointDeRencontreDistant;
+
+socklen_t adresseLongueur = sizeof(struct sockaddr_in);
+
+char messageEnvoi[LONGUEUR_MESSAGE];
+char messageRecu[LONGUEUR_MESSAGE];	
+
 struct sockaddr_in creerServeur(short family, uint16_t port) {
     struct sockaddr_in serveur;
     serveur.sin_family = PF_INET;
@@ -58,6 +77,26 @@ void *traiterClient(void *arg) {
 }
 
 int main(void) {
+	// Crée un socket de communication
+	if ((socketEcoute = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("Erreur lors de la création du socket\n");
+		return EXIT_FAILURE;
+	}
+	
+	serveur = creerServeur(PF_INET, IPPORT_USERRESERVED);
+
+	// On demande l’attachement local au socket
+	if ((bind(socketEcoute, (struct sockaddr*) &serveur, adresseLongueur)) < 0) {
+		perror("Erreur lors de l'attachement local\n");
+		return EXIT_FAILURE;
+	}
+
+	// On fixe la taille de la file d’attente à 5 (pour les demandes de connexion non encore traitées)
+	if (listen(socketEcoute, 5) < 0) {
+		perror("Erreur lors de l'écoute\n");
+		return EXIT_FAILURE;
+	}
+
     // On attend une connexion
     while (1) {
         printf("Attente d'une demande de connexion (quitter avec Ctrl-C)\n\n");
